e7dd716ee37e012437b2ca2af1cc9e286be9128e
bhimanichetan777 <135630608+bhimanichetan777@users.noreply.github.com>
2025-04-10 12:48:11 +0530

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "avatars.githubusercontent.com"],
+    domains: [
+      "localhost",
+      "avatars.githubusercontent.com",
+      "cdn.pixabay.com",
+      "i.pinimg.com",
+      "images.unsplash.com",
+      "images.hitpaw.com",
+      "media.istockphoto.com",
+      "via.placeholder.com",
+      "encrypted-tbn0.gstatic.com",
+      "photosbull.com",
+      "cutegirlpic.in",
+      "cdn.lazyshop.com",
+      "staticg.sportskeeda.com",
+      "vsthemes.org",
+    ],
   },
 };
 
